ff4f103936b313679882dc994fe67255d0e1a189
Roberto Rojas Laguna <robertorojas-laguna1@MacBook-Pro-de-Roberto-2.local>
2023-04-20 17:18:15 -0600

    Firebase

--- EQ-Web-Sort-v200/settings/firebaseInfo.js
+++ EQ-Web-Sort-v200/settings/firebaseInfo.js
@@ -1,11 +1,11 @@
 
   const firebaseConfig = {
-    apiKey: "AIzaSyAwgmKlHvFvQ9wCYr-dnL43xpMNSs1xGR4",
-    authDomain: "fortapol-taller.firebaseapp.com",
-    projectId: "fortapol-taller",
-    storageBucket: "fortapol-taller.appspot.com",
-    messagingSenderId: "629344097555",
-    appId: "1:629344097555:web:7146dfa8a7a163f96936e6"
+    apiKey: "AIzaSyAydbT6DVFV2lGprwdeKxYruDBf4xZYF3g",
+    authDomain: "habilidades-ppi.firebaseapp.com",
+    projectId: "habilidades-ppi",
+    storageBucket: "habilidades-ppi.appspot.com",
+    messagingSenderId: "860302372253",
+    appId: "1:860302372253:web:fa56469daf439038085374"
   };
   
   firebase.initializeApp(firebaseConfig);  
